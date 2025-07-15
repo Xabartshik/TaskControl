060058888200679cf9239246c31433a271e76bdf
Xabartshik <146337716+Xabartshik@users.noreply.github.com>
2025-07-15 12:36:46 +1000

    Добавлена заглушка для классан настроек AppSettings

---
+++ TaskControl.Core/AppSettings/AppSettings.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaskControl.Core.AppSettings
+{
+    //TODO: Сделать класс настроек приложения
+    public class AppSettings
+    {
+
+    }
+}
